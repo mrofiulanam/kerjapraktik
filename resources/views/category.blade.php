525c9e572786858a040ceb0ee798a5b535f9018f
mrofiulanam <rofiulanam10@gmail.com>
2023-01-16 18:05:56 +0700

    adding files

---
+++ resources/views/category.blade.php
@@ -0,0 +1,35 @@
+<style>
+  .course{
+    color: black;
+  }
+</style>
+@extends("layouts.main")
+@section("container")
+
+    <div class="container">
+        <div class="row row-cols-3"  >
+
+          @foreach($categories as $category)
+          
+          <div class="col " >
+            <div class="card mb-3 " style="max-width: 540px; border-radius: 30px;">
+            <img src="https://source.unsplash.com/500x300?{{ $category->slug }}" class="img-fluid rounded-top" alt="" >
+                 
+                <div class="card-body text-center">
+                    <h5 class="card-title fs-4 d-flex justify-content-center">{{ $category->name }}</h5>
+                    <p class="card-text">{!! $category->excerpt !!}</p>
+                    <a href="category/{{ $category->slug }}" class="  btn btn-primary px-5 ">Buka</a>
+                  </div>
+                </div>
+            
+          </div>
+          @endforeach
+          
+        </div>
+      </div>
+      
+<div class="d-flex justify-content-center">
+  {{ $categories->links() }}
+</div>
+      
+@endsection
---
+++ resources/views/feedback/index.blade.php
@@ -0,0 +1,4 @@
+@extends("layouts.main")
+@section("container")
+<p class="text-justify">Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentum.</p>
+@endsection
